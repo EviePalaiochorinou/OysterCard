96d0f9d5be8058bf05c187b791c9aa87821172b3
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2021-11-22 16:23:08 +0000

    step5

---
+++ lib/oystercard.rb
@@ -0,0 +1,15 @@
+class Oystercard
+
+attr_reader :balance
+
+def initialize
+  @balance = 0
+end
+
+def top_up(amount)
+  @balance += amount
+end
+
+
+end
+
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,6 +2,15 @@ require 'oystercard'
 
 describe Oystercard do
   
+  it 'should have a balance' do
+    expect(subject).to respond_to(:balance)
 
-  
+  end
+
+  context 'how top-up works' do
+    it 'checks if top-up increases the balance' do
+      expect{subject.top_up(5)}.to change {subject.balance}.by 5
+    end
+  end
+
 end
