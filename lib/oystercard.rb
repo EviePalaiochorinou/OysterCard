31c7c7c6bcd8e3daa0949e74a23cc97c67df68aa
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2021-11-22 16:56:51 +0000

    step6

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,13 +2,16 @@ class Oystercard
 
 attr_reader :balance
 
-def initialize
-  @balance = 0
-end
+MAXIMUM_BALANCE = 90
 
-def top_up(amount)
-  @balance += amount
-end
+  def initialize
+    @balance = 0
+  end
+
+  def top_up(amount)
+    fail "Cannot top-up more than #{MAXIMUM_BALANCE}" if (@balance + amount) > MAXIMUM_BALANCE
+    @balance += amount
+  end
 
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,15 +1,21 @@
 require 'oystercard'
 
 describe Oystercard do
-  
-  it 'should have a balance' do
-    expect(subject).to respond_to(:balance)
 
+  context 'Checks basic features of oystercard' do
+    it 'should have a balance' do
+      expect(subject).to respond_to(:balance)
+    end
   end
 
   context 'how top-up works' do
     it 'checks if top-up increases the balance' do
       expect{subject.top_up(5)}.to change {subject.balance}.by 5
+    end
+
+    it 'error if top up balance tries to go over maximum balance' do
+      subject.top_up(Oystercard::MAXIMUM_BALANCE)
+      expect{subject.top_up(1)}.to raise_error "Cannot top-up more than #{Oystercard::MAXIMUM_BALANCE}"
     end
   end
 
