417f43472bb720994f9f17099abebfb2730a9046
EviePalaiochorinou <evangeliapal@gmail.com>
2021-11-23 15:37:40 +0000

    Step 9 touch-in, touch-out, in-journey methods

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,12 @@
 class Oystercard
 
-attr_reader :balance
+attr_reader :balance, :in_journey
 
 MAXIMUM_BALANCE = 90
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -13,6 +14,26 @@ MAXIMUM_BALANCE = 90
     @balance += amount
   end
 
+  def deduct(fare)
+    @balance -= fare
+  end
 
+  def in_journey?
+    @in_journey = in_journey
+  end
+
+def touch_in
+  fail "You need to top-up first" if @balance < 1
+  @in_journey = true
+end
+
+def touch_out
+  @in_journey = false
+
+end
+
+
+
+  
 end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -19,4 +19,48 @@ describe Oystercard do
     end
   end
 
+  context 'deduct method' do
+    it 'allows the balance to be decucted' do
+      expect{subject.deduct(5)}.to change {subject.balance}.by -5
+    end
+  end
+
+  context 'touch in method' do
+    it 'checks that the user is in journey' do
+      subject.top_up(50)
+      expect(subject.touch_in).to eq true
+    end
+
+    it "can touch in" do
+      subject.top_up(50)
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+  end
+
+  context 'in journey method' do
+    it 'checks whether a card is currently being used' do
+      expect(subject.in_journey?).to eq false
+    end
+  end
+
+  context 'touch out method' do
+    it 'checks that the user is not in journey' do
+      expect(subject.touch_out).to eq false
+    end
+
+    it "can touch out" do
+      subject.top_up(50)
+      subject.touch_in
+      subject.touch_out
+      expect(subject).not_to be_in_journey
+    end
+  end
+  
+  context 'error if funds insufficient for journey' do
+    it 'checks if there are sufficient funds for a journey' do
+      expect{subject.touch_in}.to raise_error "You need to top-up first"
+    end
+  end
 end
+
